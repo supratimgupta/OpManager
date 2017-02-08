b3cb711b90e0430cfa7f7458e714eb71f64df173
supratimgupta <supratim.official@gmail.com>
2017-02-09 01:39:32 +0530

    SG - Added controller level auth

---
+++ OperationsManager/Attributes/OpMgrAuthAttribute.cs
@@ -0,0 +1,35 @@
+﻿using OperationsManager.Helpers;
+using OpMgr.Common.Contracts;
+using OpMgr.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OperationsManager.Attributes
+{
+    public class OpMgrAuthAttribute : AuthorizeAttribute
+    {
+        ISessionSvc _sessionSvc;
+        public OpMgrAuthAttribute()
+        {
+            _sessionSvc = new SessionSvc();
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            SessionDTO session = _sessionSvc.GetUserSession();
+            if(session==null)
+            {
+                return false;
+            }
+            var url = session.ActionList.FirstOrDefault(al => string.Equals(al.URL, httpContext.Request.Path));
+            if(url!=null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- OperationsManager/Controllers/HomeController.cs
+++ OperationsManager/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OperationsManager.Attributes;
 
 namespace OperationsManager.Controllers
 {
+    [OpMgrAuth]
     public class HomeController : Controller
     {
         public ActionResult Index()
