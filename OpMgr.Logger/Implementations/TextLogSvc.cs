5955a1a203877dcbdff4e33181b2d6e9365eb9b4
supratimgupta <supratim.official@gmail.com>
2017-02-22 11:09:11 +0530

    SG - Added log functionality

--- OpMgr.Logger/Implementations/TextLogSvc.cs
+++ OpMgr.Logger/Implementations/TextLogSvc.cs
@@ -4,24 +4,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace OpMgr.Logger.Implementations
 {
     public class TextLogSvc : ILogSvc
     {
         private IConfigSvc _configSvc;
+        private string _logPath;
         public TextLogSvc(IConfigSvc configSvc)
         {
             _configSvc = configSvc;
+            _logPath = configSvc.GetLogPath();
         }
+
+        private void WriteLog(string message)
+        {
+            if(!string.IsNullOrEmpty(_logPath))
+            {
+                if(!File.Exists(_logPath))
+                {
+                    File.WriteAllText(_logPath, message);
+                }
+                else
+                {
+                    File.AppendAllText(_logPath, message);
+                }
+            }
+        }
+
         public void Log(string message)
         {
-            throw new NotImplementedException();
+            string logMessage = "MESSAGE\t|\t"+DateTime.Now+"\t|\t"+message;
+            WriteLog(logMessage);
         }
 
         public void Log(Exception exception)
         {
-            throw new NotImplementedException();
+            string logMessage = "EXCEPTION\t|\t" + DateTime.Now + "\t|\t" + exception.ToString().Replace("\n","-->");
+            WriteLog(logMessage);
         }
     }
 }
