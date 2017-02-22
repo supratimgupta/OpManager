45dbf9503389774c25cefbb46eab8454aca4bfab
supratimgupta <supratim.official@gmail.com>
2017-02-22 10:51:04 +0530

    SG - Added ninject resolver for the transaction handler also and added the trans handler and logger project

---
+++ OpMgr.Logger/Implementations/TextLogSvc.cs
@@ -0,0 +1,27 @@
+﻿using OpMgr.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpMgr.Logger.Implementations
+{
+    public class TextLogSvc : ILogSvc
+    {
+        private IConfigSvc _configSvc;
+        public TextLogSvc(IConfigSvc configSvc)
+        {
+            _configSvc = configSvc;
+        }
+        public void Log(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Log(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpMgr.TransactionHandler.Runner/Program.cs
@@ -0,0 +1,38 @@
+﻿using Ninject;
+using OpMgr.Common.Contracts;
+using OpMgr.Common.Contracts.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpMgr.TransactionHandler.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string diResolverPath = System.Configuration.ConfigurationManager.AppSettings["DIResolverPath"];
+            StandardKernel kernel = new StandardKernel();
+            kernel.Load(diResolverPath);
+            try
+            {
+
+                Console.WriteLine("*** RUNNING TRANSACTION RULES ***");
+                Console.WriteLine("*** WINDOW WILL BE CLOSED AUTOMATICALLY WHEN COMPLETED ***");
+                Console.WriteLine("*** PLEASE DON'T CLOSE WINDOW ***");
+
+                ITransactionSvc trnsSvc = kernel.Get<ITransactionSvc>();
+                trnsSvc.AddRegularTransactions();
+                trnsSvc.CheckDuesAndAddFine();
+                trnsSvc.CheckLibraryDueAndAddFine();
+            }
+            catch(Exception exp)
+            {
+                ILogSvc logger = kernel.Get<ILogSvc>();
+                logger.Log(exp);
+            }
+        }
+    }
+}
