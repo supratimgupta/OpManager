723507b0eb1d87899af3d714eaddbadeeab798ca
Suvendu Pakui <Suvendu Pakui@SUVENDU>
2017-10-08 19:48:31 +0530

    worked for pms

---
+++ OperationsManager/Areas/PMS/Controllers/PMSController.cs
@@ -0,0 +1,24 @@
+﻿using OperationsManager.Controllers;
+using OpMgr.Common.Contracts.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OperationsManager.Areas.PMS.Controllers
+{
+    public class PMSController : BaseController
+    {
+        private IUserSvc _userSvc;
+        //private ILogSvc _logSvc;
+        private IDropdownRepo _dropDownRepo;
+        private Helpers.UIDropDownRepo _uiddlRepo;
+
+        [HttpGet]
+        public ActionResult GoalSheetForAll()
+        {
+            return View();
+        }
+    }
+}
---
+++ OperationsManager/Areas/PMS/Models/GoalViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OpMgr.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OperationsManager.Areas.PMS.Models
+{
+    public class GoalViewModel
+    {
+        public List<GoalAttributeDTO> GoalAttributes { get; set; }
+    }
+}
