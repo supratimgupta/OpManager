b8ed037c5bece1c08aa6dfd4f9f7ae10ef287145
supratimgupta <supratim.official@gmail.com>
2018-04-27 16:25:07 +0530

    SG

---
+++ OpMgr.Common/Contracts/Modules/IResultSvc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpMgr.Common.Contracts.Modules
+{
+    public interface IResultSvc
+    {
+        DataTable GetResultFormat(int standardSectionId, int examTypeId);
+
+        DataTable GetResultRule(int standardSectionId);
+
+        DataTable GetGradeConfig();
+
+        DataTable GetStudentResults(int standardSectionId, List<int> examTypes, DateTime academicStartDate, DateTime academicEndDate);
+    }
+}
