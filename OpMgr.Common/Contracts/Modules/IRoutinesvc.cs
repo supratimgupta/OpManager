93a4b783f3cfd359c5a0ec4309b295120c159cd5
sourya_chats <souryachats@gmail.com>
2019-01-19 15:59:02 +0530

    Routine Management

---
+++ OpMgr.Common/Contracts/Modules/IRoutinesvc.cs
@@ -0,0 +1,21 @@
+﻿using OpMgr.Common.Contracts;
+using OpMgr.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpMgr.Common.Contracts.Modules
+{
+   public interface IRoutinesvc 
+    {
+        List<RoutineTable> generateTable(int id,int location);
+        AddEditRoutine GetData(int id);
+        AddEditRoutine PostData(AddEditRoutine rtnedit);
+        List<subject> getSubject(string term);
+        List<Employee> getEmployee(string term,int location);
+        List<Employee> GetReplacementTeacherList(int id);
+
+    }
+}
